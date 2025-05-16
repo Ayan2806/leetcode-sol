ec0d8751b6f9a17b419bf7d06a84be8516b4dfd8
Ayan2806 <ayan.rastogi28@gmail.com>
2025-05-16 10:28:12 +0530

    Time: 1 ms (98.62%), Space: 42.2 MB (75.03%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int e=0;
+        int o=0;
+        for(int i=0;i<num.length();i++){
+            int d=num.charAt(i)-'0';
+            if(i%2==0){
+                e+=d;
+            }
+            else{
+                o+=d;
+            }
+        }
+        return e==o;
+    }
+}
